d8e0729bbb0ea5bc2f223f1b6a0aa8f3b64a842e
Farrukh Saleem Sheikh <imfarrukhsaleem@hotmail.com>
2022-06-05 23:21:43 +0500

    Add project files.

---
+++ Apple.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Apple.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Apple.Services.ProductAPI.DbContexts
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+		{
+
+		}
+		public DbSet<Product> Products { get; set; }
+	}
+}
---
+++ Apple.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Apple.Services.ProductAPI.Models.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Apple.Services.ProductAPI.Repository
+{
+	public interface IProductRepository
+	{
+		Task<IEnumerable<ProductDto>> GetProducts();
+		Task<ProductDto> GetProductById(int ProductId);
+		Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+		Task<bool> DeleteProduct(int ProductId);
+	}
+}
